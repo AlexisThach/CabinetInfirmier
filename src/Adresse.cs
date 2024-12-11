793eb6e1316961fe64b9100925ec515b83b843f8
AlexisThach <thyden26@gmail.com>
2024-12-11 17:10:12 +0100

    Parser XMLReader fini

---
+++ src/Adresse.cs
@@ -0,0 +1,10 @@
+namespace CabinetInfirmier;
+
+public class Adresse {
+    
+    public string Nom { get; set; }
+    public string Rue { get; set; }
+    public string Ville { get; set; }
+    public string CodePostal { get; set; }
+    
+}
--- src/Program.cs
+++ src/Program.cs
@@ -2,14 +2,24 @@ using CabinetInfirmier;
 
 AppContext.SetSwitch("Switch.System.Xml.AllowDefaultResolver", true);
 
-// Tests de validation de fichiers XML
-//XMLUtils.ValidateXmlFileAsync("http://www.univ-grenoble-alpes.fr/l3miage/medical","./src/data/xsd/cabinet.xsd","./src/data/xml/cabinet.xml");
-//XMLUtils.ValidateXmlFileAsync("http://www.univ-grenoble-alpes.fr/l3miage/medical","./src/data/xsd/patient.xsd","./src/data/xml/patient.xml");
+string pathXmlCabinet = "./src/data/xml/cabinet.xml";
+string pathXmlPatient = "./src/data/xml/patient.xml";
+string pathXmlActes = "./src/data/xml/actes.xml";
+
+// Tests de validation de fichiers XMLSchema
+//XMLUtils.ValidateXmlFileAsync("http://www.univ-grenoble-alpes.fr/l3miage/medical","./src/data/xsd/cabinet.xsd", pathXmlCabinet);
+//XMLUtils.ValidateXmlFileAsync("http://www.univ-grenoble-alpes.fr/l3miage/medical","./src/data/xsd/patient.xsd", pathXmlPatient);
 
 // Tests de transformation XSLT
-//XMLUtils.XslTransform("./src/data/xml/cabinet.xml", "./src/data/xslt/cabinet.xsl", "./src/data/html/cabinet_test.html");
-//XMLUtils.XslTransform("./src/data/xml/patient.xml", "./src/data/xslt/patient_html.xsl", "./src/data/html/patient_test.html");
+//XMLUtils.XslTransform(pathXmlCabinet, "./src/data/xslt/cabinet.xsl", "./src/data/html/cabinet_test_cs.html");
+//XMLUtils.XslTransform(pathXmlPatient, "./src/data/xslt/patient_html.xsl", "./src/data/html/patient_test_cs.html");
 
-// Parsers 
+// Parser XMLReader
+//CabinetXmlReader.AnalyseGlobal(pathXmlCabinet);
+//CabinetXmlReader.GetTexteFromElements(pathXmlCabinet , "nom", "infirmiers");
+//CabinetXmlReader.GetNbActes(pathXmlActes);
+
+
+
 
 
